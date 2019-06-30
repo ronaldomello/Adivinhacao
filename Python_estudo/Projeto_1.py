eae67e5b59fab0b36555f56d84bdb7c6ef725acc
ronaldomello <51142877+ronaldomello@users.noreply.github.com>
2019-06-30 19:49:13 -0300

    Add files via upload

--- Python_estudo/Projeto_1.py
+++ Python_estudo/Projeto_1.py
@@ -1,16 +1,32 @@
-print("**********************************")
-print("bem vindo no jogo de adivinhação!")
-print("**********************************")
 
-numero_secreto = 42
+#Este jogo consiste em acertar o número secreto, envolvendo várias regras simples
 
-chute_str = input("Digite o seu número: ")
-print("Você digitou: ", chute_str)
-chute = int(chute_str)
+print("***********************************")
+print("*bem vindo no jogo de adivinhação!*")
+print("***********************************")
 
-if (numero_secreto == chute):
-    print("Você acertou!")
-else:
-    print("Você errou!")
+numero_secreto = 10
+numero_de_tentativas = 3
+rodada = 1
+
+while(rodada <= numero_de_tentativas):
+    print("tentativa {} de {}".format(rodada, numero_de_tentativas))
+    chute_str = input("Digite o seu número: ")
+    print("Você digitou: ", chute_str)
+    chute = int(chute_str)
+
+    acertou = numero_secreto == chute
+    maior   = numero_secreto < chute
+    menor   = numero_secreto > chute
+
+    if (acertou):
+        print("Você acertou!")
+    else:
+        if(maior):
+            print("Seu chute ultrapassou o número secreto!")
+        elif(menor):
+            print("seu chute foi menor que o nmero secreto")
+    rodada = rodada + 1
+    
 
 print("Fim do Jogo!")
